893b693ddb2da9e9054c9f220ac4638b04de344c
Elvin Pineda <isaacmine512@gmail.com>
2026-04-23 14:31:52 -0400

    Fixing some of my tests and adding the poly mod function

--- aes/helperFunctions.py
+++ aes/helperFunctions.py
@@ -1,13 +1,33 @@
 ## Holding all of the helper functions for AES shared between groups
 
-# We need 
-
-## Calculate Degree of Poly
 def getDegree(a):
-    for i in range(7, -1, -1):
+    """
+    This gets the degree of any polynomial represented as an integer. 
+    an example is 0x15 which is 0001 0101 in binary which 
+    can be written as x^4 + x^2 + 1; this has a degree of 4.
+    
+    Args: the polynomial such as 0x01 or 0001 
+    Returns: the degree of the polynomial, -1 if the polynomial is 0
+    """
+    for i in range(15, -1, -1):
         if (a >> i) & 1:
             return i
     return -1
+
+## Calculating polynomial modulo 
+def polyMod(a, m):
+    """
+    This calculates the polynomial modulo of a by m. 
+    a gets divided by m and the remainder is returned.
+    
+    args: a and m are polynomials as integers
+    returns the remainder of a divided by m as an integer
+    """
+    if (getDegree(a) == -1 or getDegree(m) == -1):
+        return 0
+    while getDegree(a) >= getDegree(m):
+        a ^= m << (getDegree(a) - getDegree(m))
+    return a
 ## Calculate Modular Inverse over arbitrary m(x)
 
 ## Calculate Polynomial Multiplication over arbitrary m(x)
