be5cb031a134f34a7dd9494245053bb2811b60f1
Elvin Pineda <isaacmine512@gmail.com>
2026-04-24 15:13:05 -0400

    finishing sbox

--- aes/sBox.py
+++ aes/sBox.py
@@ -1,4 +1,4 @@
-from helperFunctions import * 
+from aes.helperFunctions import * 
 """
 This file is the subBox file that converts or substitutes those bytes needed
 for the AES encryption
@@ -16,7 +16,7 @@ def subByte(a):
     s = b ^ rotate(b, 1) ^ rotate(b,2) ^ rotate(b,3) ^ rotate(b,4) ^ 0x63
     #  The line above xors the mod inverse by itself but at different circular rotations
     #  Then finally it xored with 0x63 which is the constant.
-    return s ^ 0xFF
+    return s & 0xFF
 
 def rotate(b, n):
     """
@@ -26,3 +26,26 @@ def rotate(b, n):
     return ((b << n) | (b >> (8 - n))) & 0xFF # This shifts the bits circular but keeps it below 8 bits 
 
 ## Next on list is to create the 2D array that holds all of that subbytes :(
+def generateSBox ():
+    """
+    This is generating the sBox for in a 16x16 array
+    """
+    sBox = [[0] * 16 for _ in range (16)] # Creates a 2D array that we fill in
+    for i in range(0, 256):
+        sBox[i//16][i%16] = subByte(i)
+    return sBox
+def printSBox(sBox):
+    for row in sBox:
+        for val in row:
+            print(f"{val:02x}", end=" ")
+        print()
+
+def generateInvSBox():
+    sBox = generateSBox()
+    invSBox = [[0] * 16 for _ in range (16)] # Creates a 2D array that we fill in
+
+    for i in range (256):
+        currentValue = sBox[i//16][i%16] 
+        invSBox[currentValue //16][currentValue%16] = i # this line takes the current value were in 
+        # and then determines the row and column then places it in its spot respectively for example index 0x00 has value 63 it put 00 in row 6 col 3
+    return invSBox
---
+++ tests/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+from aes.sBox import *
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+printSBox(generateSBox())
