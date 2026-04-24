79c1f5fc114daf09d22eb399ea2d0ecbbf22bf45
Elvin Pineda <isaacmine512@gmail.com>
2026-04-24 17:43:19 -0400

    init branch

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-## Will encrypt and decrypt the message ## 
+import sys
+from aes.sBox import *
+from aes.mixColumns import *
+# from aes.keyExpansion import *  # stub until teammate finishes
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python main.py <key> <input_file> <output_file>")
+        return
+    
+    key = sys.argv[1]
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
